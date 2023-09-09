0834c2b8e40967d4521eb1e59c0dfbdf98d5f4b7
richflor <fld.richard@gmail.com>
2023-09-09 02:02:57 +0200

    re 4

--- src/component/Products.tsx
+++ src/component/Products.tsx
@@ -17,7 +17,7 @@ export function Products () {
             sx={{
                 width: "100vw",
                 paddingBlock: "5vh",
-                minHeight: "80vh",
+                minHeight: "86vh",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
